4bc9f3396f4199608af31e402e981d7ca17316f0
remyd95 <remyduijsens@gmail.com>
2023-10-05 09:53:31 +0200

    Let the ball update the current action believe

--- client/devices/ball.py
+++ client/devices/ball.py
@@ -25,16 +25,12 @@ class Ball:
     def action(self, action_type, data=None):
         if action_type == ActionType.FORWARD:
             self.mqtt_connector.publish(self.topic, f"FW {data['speed']}")
-            self.cur_action = ActionType.FORWARD
         elif action_type == ActionType.BACKWARD:
             self.mqtt_connector.publish(self.topic, f"BW {data['speed']}")
-            self.cur_action = ActionType.BACKWARD
         elif action_type == ActionType.STOP:
             self.mqtt_connector.publish(self.topic, "ST")
-            self.cur_action = None
         elif action_type == ActionType.MOVETO:
             self.mqtt_connector.publish(self.topic, f"MV {data['x']} {data['y']} {data['speed']}")
-            self.cur_action = ActionType.MOVETO
 
     def set_gui_object(self, gui_obj):
         self.gui_obj = gui_obj
