5dad504c133ff6b73c5fd503e56d0c9b83a620b8
remyd95 <remyduijsens@gmail.com>
2023-10-31 02:00:47 +0100

    Modify state and target from action handler

--- include/action_handler.h
+++ include/action_handler.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "motor_action_data.h"
 #include "pwm_motor.h"
 
 void process_action(char* event_data);
--- src/action_handler.c
+++ src/action_handler.c
@@ -9,124 +9,68 @@
 
 
 void process_action(char* event_data){
-    motor_action_data_t motor_action_data;
-    motor_action_data.motor_id = MOTOR_ALL;
-
     if (strncmp(event_data, "FW", 2) == 0) {
-        float speed_left;
-        float speed_right;
+        float target_speed;
 
-        if (sscanf(event_data, "FW %f %f", &speed_left, &speed_right) == 2) {
-            float bounded_speed_left = bound_max_speed(speed_left);
-            float bounded_speed_right = bound_max_speed(speed_right);
-
-            motor_action_data.speed_left = bounded_speed_left;
-            motor_action_data.speed_right = bounded_speed_right;
-
-            motor_action_data.speed_left_max = bounded_speed_left;
-            motor_action_data.speed_right_max = bounded_speed_right;
-
+        if (sscanf(event_data, "FW %f", &target_speed) == 1) {
+            float bounded_target_speed = bound_max_speed(target_speed);
             set_current_action(ACTION_FORWARD);
-            //forward_callback(motor_action_data);
+            set_target_speed(bounded_target_speed);
         }
     } else if (strncmp(event_data, "BW", 2) == 0) {
-        float speed_left;
-        float speed_right;
+        float target_speed;
 
-        if (sscanf(event_data, "BW %f %f", &speed_left, &speed_right) == 2) {
-            float bounded_speed_left = bound_max_speed(speed_left);
-            float bounded_speed_right = bound_max_speed(speed_right);
-
-            motor_action_data.speed_left = bounded_speed_left;
-            motor_action_data.speed_right = bounded_speed_right;
-
-            motor_action_data.speed_left_max = bounded_speed_left;
-            motor_action_data.speed_right_max = bounded_speed_right;
-
+        if (sscanf(event_data, "BW %f", &target_speed) == 1) {
+            float bounded_target_speed = bound_max_speed(target_speed);
             set_current_action(ACTION_BACKWARD);
-            //backward_callback(motor_action_data);
+            set_target_speed(bounded_target_speed);
         }
     } else if (strncmp(event_data, "TL", 2) == 0) {
-        float speed_left;
-        float speed_right;
+        float target_speed;
 
-        if (sscanf(event_data, "TL %f %f", &speed_left, &speed_right) == 2) {
-            float bounded_speed_left = bound_max_speed(speed_left);
-            float bounded_speed_right = bound_max_speed(speed_right);
-
-            motor_action_data_t motor_action_data1;
-            motor_action_data_t motor_action_data2;
-            
-            motor_action_data1.speed_left = bounded_speed_left;
-            motor_action_data2.speed_left = bounded_speed_left;
-            motor_action_data1.speed_right = bounded_speed_right;
-            motor_action_data2.speed_right = bounded_speed_right;
-
-            motor_action_data1.speed_left_max = bounded_speed_left;
-            motor_action_data2.speed_left_max = bounded_speed_left;
-            motor_action_data1.speed_right_max = bounded_speed_right;
-            motor_action_data2.speed_right_max = bounded_speed_right;
-
+        if (sscanf(event_data, "TL %f", &target_speed) == 1) {
+            float bounded_target_speed = bound_max_speed(target_speed);
             set_current_action(ACTION_TURN_LEFT);
-            motor_action_data1.motor_id = MOTOR_LEFT;
+            set_target_speed(bounded_target_speed);
+
+            // OLD CODE - Keep for reference
+            //motor_action_data1.motor_id = MOTOR_LEFT;
             //backward_callback(motor_action_data1);
-            motor_action_data2.motor_id = MOTOR_RIGHT;
+            //motor_action_data2.motor_id = MOTOR_RIGHT;
             //forward_callback(motor_action_data2);
         }
     } else if (strncmp(event_data, "TR", 2) == 0) {
-        float speed_left;
-        float speed_right;
+        float target_speed;
 
-        if (sscanf(event_data, "TR %f %f", &speed_left, &speed_right) == 2) {
-            float bounded_speed_left = bound_max_speed(speed_left);
-            float bounded_speed_right = bound_max_speed(speed_right);
-
-            motor_action_data_t motor_action_data1;
-            motor_action_data_t motor_action_data2;
-            
-            motor_action_data1.speed_left = bounded_speed_left;
-            motor_action_data2.speed_left = bounded_speed_left;
-            motor_action_data1.speed_right = bounded_speed_right;
-            motor_action_data2.speed_right = bounded_speed_right;
-
-            motor_action_data1.speed_left_max = bounded_speed_left;
-            motor_action_data2.speed_left_max = bounded_speed_left;
-            motor_action_data1.speed_right_max = bounded_speed_right;
-            motor_action_data2.speed_right_max = bounded_speed_right;
-            
+        if (sscanf(event_data, "TR %f", &target_speed) == 1) {
+            float bounded_target_speed = bound_max_speed(target_speed);
             set_current_action(ACTION_TURN_RIGHT);
-            motor_action_data1.motor_id = MOTOR_LEFT;
+            set_target_speed(bounded_target_speed);
+
+            // OLD CODE - Keep for reference
+            //motor_action_data1.motor_id = MOTOR_LEFT;
             //forward_callback(motor_action_data1);
-            motor_action_data2.motor_id = MOTOR_RIGHT;
+            //motor_action_data2.motor_id = MOTOR_RIGHT;
             //backward_callback(motor_action_data2);
         }
     } else if (strncmp(event_data, "ST", 2) == 0) {
         set_current_action(ACTION_STOP);
-        //stop_callback(motor_action_data);
-        set_current_action(ACTION_IDLE); // TODO: Only set to action idle if we know the velocity is 0
-
+        set_target_speed(0.0);
     } else if (strncmp(event_data, "MT", 2) == 0) {
-        float x, y;
+        float x, y, target_speed;
 
-        float speed_left;
-        float speed_right;
-        if (sscanf(event_data, "MT %f %f %f %f", &x, &y, &speed_left, &speed_right) == 4) {
+        if (sscanf(event_data, "MT %f %f %f", &x, &y, &target_speed) == 3) {
             // Change action data to contains these args?
-            float bounded_speed_left = bound_max_speed(speed_left);
-            float bounded_speed_right = bound_max_speed(speed_right);
+            float bounded_target_speed = bound_max_speed(target_speed);
             set_current_action(ACTION_MOVETO);
-            //move_to(x, y, speed_left, speed_right);
+            set_target_coordinates(x, y);
+            set_target_speed(bounded_target_speed);
         }
     } else if (strncmp(event_data, "IN", 2) == 0) {
         float x, y;
         if (sscanf(event_data, "IN %f %f", &x, &y) == 2) {
             set_current_coordinates(x, y);
             set_current_action(ACTION_IDLE); // After initialization set to idle
-        }
-    } else if (strncmp(event_data, "FA", 2) == 0) {
-        int client_id;
-        if (sscanf(event_data, "FA %d", &client_id) == 2) {
-            //mqtt_publish_message(mqtt_client, message);
         }
     } else if (strncmp(event_data, "RB", 2) == 0) {
         esp_restart();
